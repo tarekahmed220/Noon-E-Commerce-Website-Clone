59cebd5f4269f2677f75046b6eb159e384b41135
tarekahmed220 <tarek.ahm12@gmail.com>
2024-08-05 00:29:39 +0300

    complete navbar UI with responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { FlexCenterDirective } from '../../CutomeDirectives/flex-center.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FlexCenterDirective],
+  imports: [FlexCenterDirective, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  showLinks: boolean = false;
+
+  changeShowLinksStatus() {
+    this.showLinks = !this.showLinks;
+  }
+}
