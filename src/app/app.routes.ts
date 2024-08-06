73ecaba11f4649256ad6779105d5609203127d78
tarekahmed220 <tarek.ahm12@gmail.com>
2024-08-06 16:26:17 +0300

    footer-login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'signin', component: SignInComponent }];
--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IUser } from '../../models/IUser';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrl: './sign-in.component.css',
 })
 export class SignInComponent {
-
+  signinData: IUser = {} as IUser;
+  isLoggedIn: boolean = true;
+  onLogin() {
+    this.isLoggedIn = !this.isLoggedIn; // Toggles the value of isLoggedIn
+  }
 }
