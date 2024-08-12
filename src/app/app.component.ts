c91b89ea7b5c6e1a296200eb6c0338d976d7dcda
mohammedhosni25@gmail.com <mohammedhosni25@gmail.com>
2024-08-12 15:03:44 +0300

    static auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignInComponent } from "./components/sign-in/sign-in.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SignInComponent, FooterComponent, SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-export const routes: Routes = [{ path: 'signin', component: SignInComponent }];
+export const routes: Routes = [{ path: 'signin', component: SignInComponent },
+    { path: 'signup', component: SignUpComponent }
+];
