2bb5a2f53c51202346a77878f365d3ec58b5e6e3
tarekahmed220 <tarek.ahm12@gmail.com>
2024-08-12 17:55:10 +0300

    modify navbar-app components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [{ path: 'signin', component: SignInComponent }];
+export const routes: Routes = [
+  { path: 'signin', component: SignInComponent },
+  { path: 'home', component: LandingPageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PagenotfoundComponent },
+];
