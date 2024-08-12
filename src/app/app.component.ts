2d36c706fcc082397ebc88f5932c3e2921778664
abosaifzzz <mohamedabosaif21@gmail.com>
2024-08-13 01:24:12 +0300

    product-details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { SignInComponent } from "./components/sign-in/sign-in.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { SignUpComponent } from "./components/sign-up/sign-up.component";
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SignInComponent, FooterComponent, SignUpComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    SignInComponent,
+    FooterComponent,
+    SignUpComponent,
+    ProductDetailsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
-
+  window: any;
 }
