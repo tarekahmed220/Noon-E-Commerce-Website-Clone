e4958c97a08f3d66fdccfa0fabcf0d96c9791ba0
abosaifzzz <mohamedabosaif21@gmail.com>
2024-08-22 22:26:15 +0300

    product details update with api and not found page and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,24 @@
-import { Routes } from '@angular/router';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 export const routes: Routes = [
-  { path: 'signin', component: SignInComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect root to 'home'
   { path: 'home', component: LandingPageComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-   { path: 'signup', component: SignUpComponent },
+  { path: 'signin', component: SignInComponent }, // Changed from '' to 'signin'
+  { path: 'signup', component: SignUpComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
   { path: '**', component: PagenotfoundComponent },
-
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
