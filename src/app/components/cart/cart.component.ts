7d4286cfbcf9592818fb145c305cf62393393a04
AhmedTarek22 <ahmedtarekabdo22@gmail.com>
2024-08-23 06:02:45 +0300

    update cart page and add cartService and add IProduct

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { CartProductComponent } from "./cart-product/cart-product.component";
+import { IProduct } from '../../interface/IProduct';
+import { CartService } from '../../Services/cart.service';
+import { ProductCartComponent } from './cart-product/cart-product.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CartProductComponent],
+  imports: [ProductCartComponent,],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+
+  productsCart: IProduct[] = [];
+  totalItems: number = 0;
+  subTotal: number = 0;
+
+
+  constructor(private cartServ: CartService){}
+
+  ngOnInit(): void {
+    this.subTotal = this.cartServ.getSubTotal();
+    console.log(this.subTotal);
+
+    this.totalItems = this.cartServ.getTotalItems();
+  }
 
 }
